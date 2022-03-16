fb6164b7123b0ee0191691a2c13027364ca1988b
F3NJ0 <58293913+F3NJ0@users.noreply.github.com>
2022-03-16 10:58:02 +0100

    Connecting to femmovies api
    Set movies state from /movies endpoint

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,16 +9,24 @@ class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      // Creating a list of movies for testing purposes
-      movies: [
-        { _id: 1, Title: 'Eternals', Description: 'desc1...', ImagePath: '...' },
-        { _id: 2, Title: 'Little Women', Description: 'desc2...', ImagePath: '...' },
-        { _id: 3, Title: 'The Farewell', Description: 'desc3...', ImagePath: '...' }
-      ],
-
+      // Creating an empty array to hold movie data from database
+      movies: [],
       // Set selectedMovie to null in the beginning, will be used to open MovieView component
       selectedMovie: null
     };
+  }
+
+  // Query femmovies API /movies endpoint to set movies state
+  componentDidMount() {
+    axios.get('https://femmovies.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   // Create function to set the state of selectedMovie to the newSelectedMovie passed in onMovieClick and onBackClick props
@@ -29,18 +39,18 @@ class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-    // If movie list is empty, display default message
-    if (movies.length === 0) return <div className="main-view">This list is empty!</div>;
+    // If movie list is empty (while movies load from API), display empty page
+    if (movies.length === 0) return <div className="main-view" />;
 
     // Else, logic to display the main-view: 
     // If no movie is selected (selecteMovie = null), display a MovieCard for each movie in the list
-    // If a movie is selected (via setSelectedMovie), display MovieView for this movie
+    // If a movie is selected (via setSelectedMovie, clicking on the MovieCard), display MovieView for this movie
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} /> // onBackClick prop is used in movie-view <button> element as function passed to onClick() event listener
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} /> // onMovieClick prop is used in movie-card <div> element as function passed to onClick() event listener
+            <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie) }} /> // onMovieClick prop is used in movie-card <div> element as function passed to onClick() event listener
           ))
         }
       </div>
